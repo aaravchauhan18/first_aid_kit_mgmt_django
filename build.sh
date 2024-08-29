1a6c272cb4e0d5305907f3c7d2c7841be31f500b
Aarav Chauhan <82814692+aaravchauhan18@users.noreply.github.com>
2024-08-29 08:28:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,9 @@
-# echo "Building the project..."
-# python 3.12 -m pip install -r requirements.txt
-
-# echo "Make Migration..."
-# python 3.12 manage.py makemigrations --noinput
-# python 3.12 manage.py migrate --noinput
-
-# echo "Collect Static..."
-# python 3.12 manage.py collectstatic --noinput --clear
-
-
 echo "Building the project..."
-python 3.12.5 -m pip install -r requirements.txt
+python 3.12 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python 3.12.5 manage.py makemigrations --noinput
-python 3.12.5 manage.py migrate --noinput
+python 3.12 manage.py makemigrations --noinput
+python 3.12 manage.py migrate --noinput
 
 echo "Collect Static..."
-python 3.12.5 manage.py collectstatic --noinput --clear
-
+python 3.12 manage.py collectstatic --noinput --clear
