02b247a9315e6da87e969050c717bbb24206fa36
Aarav Chauhan <82814692+aaravchauhan18@users.noreply.github.com>
2024-08-29 08:21:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,25 @@
-echo "Building the project..."
-python 3.12 -m pip install -r requirements.txt
+# echo "Building the project..."
+# python 3.12 -m pip install -r requirements.txt
+
+# echo "Make Migration..."
+# python 3.12 manage.py makemigrations --noinput
+# python 3.12 manage.py migrate --noinput
+
+# echo "Collect Static..."
+# python 3.12 manage.py collectstatic --noinput --clear
+
+
+#!/bin/bash
+
+echo "Installing Python..."
+apt-get update
+apt-get install -y python3 python3-pip
 
 echo "Make Migration..."
-python 3.12 manage.py makemigrations --noinput
-python 3.12 manage.py migrate --noinput
+python3 manage.py makemigrations
+
+echo "Apply Migrations..."
+python3 manage.py migrate
 
 echo "Collect Static..."
-python 3.12 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput
