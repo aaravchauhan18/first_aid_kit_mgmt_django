8b4cbc96d429b41594f8bc5e00779785ded22fed
aaravchauhan18 <aaravchauhan2211@gmail.com>
2025-12-25 11:06:47 +0530

    update the email key and db

--- first_aid_kit_mgmt/settings.py
+++ first_aid_kit_mgmt/settings.py
@@ -142,6 +142,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'firstaidkitmgmt@gmail.com'  # Your email
-EMAIL_HOST_PASSWORD = 'sflofzwwllsmvrgw'  # Your email password
+EMAIL_HOST_PASSWORD = 'huxrmcerxkbkrlor'  # Your email password
 
 
